680a3540b950d6747010b000784169e9643c1113
gitwanci <95022786+gitwanci@users.noreply.github.com>
2021-11-26 15:22:45 +0800

    pull commit

--- src/main/java/com/kuang/GitTest.java
+++ src/main/java/com/kuang/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello Git 2");
         System.out.println("hello Git 3");
         System.out.println("push test!");
+        System.out.println("pull test!");
 
     }
 
